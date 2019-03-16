e8891989da70b43967d9516bf78142f9d9c931c1
chrishoen <chris123hoen@gmail.com>
2019-03-16 18:30:22 -0400

    more

---
+++ RisLib/aaa_ProtoString.bat
@@ -0,0 +1,2 @@
+start "String1" C:\Users\chris\CMakeBuilds\Dev_RisLib\ProtoString\ProtoString.exe UdpPeer1
+start "String2" C:\Users\chris\CMakeBuilds\Dev_RisLib\ProtoString\ProtoString.exe UdpPeer2
