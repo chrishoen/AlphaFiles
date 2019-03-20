8c3e037f07a61340397ce298622eece19044d6f4
chrishoen <chris123hoen@gmail.com>
2019-03-20 14:23:51 -0400

    more

---
+++ FrontEnd/aaa_copy_FrontEnd.bat
@@ -0,0 +1,4 @@
+cd /d %~dp0
+scp C:\Prime\DevelopComm\Dev_FrontEnd\FrontEnd\bin\Release\FrontEnd.exe linaro@tboardx:/opt/mono
+scp C:\Prime\DevelopComm\Dev_FrontEnd\FrontEnd\bin\Release\RisLib.dll linaro@tboardx:/opt/mono
+pause
---
+++ RisLibCs/aaa_copy_files.bat
@@ -0,0 +1,4 @@
+cd /d %~dp0
+scp C:\Prime\DevelopComm\Dev_RisLibCs\Test\bin\Release\Test.exe linaro@tboardx:/opt/mono
+scp C:\Prime\DevelopComm\Dev_RisLibCs\Test\bin\Release\RisLib.dll linaro@tboardx:/opt/mono
+pause
