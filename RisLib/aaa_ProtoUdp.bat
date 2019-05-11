8e44afda9b176fa6082b190b4873355649c34ea2
chrishoen <chris123hoen@gmail.com>
2019-05-11 18:24:51 -0400

    more

---
+++ RisLib/aaa_ProtoUdp.bat
@@ -0,0 +1,4 @@
+start "UDP1" C:\Users\chris\CMakeBuilds\Dev_RisLib\ProtoUdp\ProtoUdp.exe UdpPeer1
+start "UDP2" C:\Users\chris\CMakeBuilds\Dev_RisLib\ProtoUdp\ProtoUdp.exe UdpPeer2
+
+
--- RisLib/aaaa_ProtoUdp.bat
+++
@@ -1,4 +0,0 @@
-rem start "UDP1" C:\Users\chris\CMakeBuilds\Dev_RisLib\ProtoUdp\ProtoUdp.exe UdpPeer1
-start "UDP2" C:\Users\chris\CMakeBuilds\Dev_RisLib\ProtoUdp\ProtoUdp.exe UdpPeer2
-
-
